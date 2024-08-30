a23a9fa9f4fc270f51bda9af3603c92b3b627354
Emery Li <71569536+Emeryli@users.noreply.github.com>
2024-08-30 13:23:07 -0700

    Add file structure and implement logger.py and exception.py

---
+++ src/logger.py
@@ -0,0 +1,24 @@
+# import
+import os
+import logging
+from datetime import datetime
+
+# Log set up
+LOG_FILE= f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path= os.path.join(os.getcwd(), "logs", LOG_FILE)
+os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+
+# Basic Config
+logging.basicConfig(
+    filename=log_path,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# Test
+def main():
+     logging.info("Logging has started")
+
+if __name__=="__main__":
+   main()
